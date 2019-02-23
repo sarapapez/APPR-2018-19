ca7da8cc80f512037521042aed1f8f11651acef3
Sara Papez <papezsara@gmail.com>
2019-02-23 14:52:51 +0100

    dodala tabelo

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -31,6 +31,12 @@ G <- read_csv2("podatki/stevilo_zaposlenih_v_podjetjih1.csv", col_names=c("regij
                                                grouping_mark = "."),
                n_max = 12)
 
+#S <- read_csv2("podatki/stopnja_brezposelnosti.csv", col_names=c("regije", 2006:2016),
+ #                   skip=5, na="...", locale=locale(encoding="Windows-1250", 
+  #                                                  decimal_mark = ",",
+   #                                                 grouping_mark = "."),
+    #                n_max = 12)
+
 
 
 stevilo_podjetij <- melt(M, id.vars="regije", measure.vars=names(M)[-1], variable.name="leto", 

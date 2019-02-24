d05ddbb51dc33f23fe935b52993553d04854e92e
Sara Papez <papezsara@gmail.com>
2019-02-24 15:52:00 +0100

    grafi

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,14 +1,22 @@
-# 3. faza: Vizualizacija podatkov
+#graf.stevila.podjetij <- ggplot(stevilo_podjetij, filter(leto == 2006)) + 
+ # aes(x=regije,y=stevilo) + geom_col() +
+  #theme(axis.text.x = element_text(angle = 90, vjust = 0.3, hjust=1))
 
-# Uvozimo zemljevid.
-zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip", "OB",
-                             pot.zemljevida="OB", encoding="Windows-1250")
-levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
-  { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
-zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels=levels(obcine$obcina))
-zemljevid <- fortify(zemljevid)
+#graf.stevila.podjetij <- ggplot(stevilo_podjetij) + 
+ # aes(x=regije, y=stevilo) + geom_col() + xlab("Statistične regije") + ylab("Nastali odpadki (kg na prebivalca)") +
+  #ggtitle("Stolpični diagram nastalh odpadkov v letu 2010 za statistične regije Slovenije") + 
+  #theme(axis.text.x = element_text(angle = 90, vjust = 0.3, hjust=1))
 
-# Izračunamo povprečno velikost družine
-povprecja <- druzine %>% group_by(obcina) %>%
-  summarise(povprecje=sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
 
+
+#ggplot(stevilo_podjetij_ki_so_prenehala_z_delovanjem, filter(leto == 2010), aes(x=leto, y=stevilo, group=regije, color=regije)) + geom_line() + xlab("Leto") + ylab("Število podjetij, ki so prenehala z delovanjem") + ggtitle("Graf števila propadlih podjetij") + scale_color_discrete(name="Regije", labels=c("Gorenjska", "Goriška", "Jugovzhodna Slovenija", "Koroška", "Obalno-kraška", "Osrednjeslovenska", "Podravska", "Pomurska", "Posavska", "Primorsko-notranjska", "Savinjska", "Zasavska")) + theme(axis.text.x = element_text(angle = 90, vjust = 0.3, hjust=1))
+
+#graf.loceni.odpadki.regije.2002 <- ggplot(stevilo_podjetij, filter(regije != 'Pomurska', leto == 2007)) + 
+ # aes(x=leto, y=stevilo) + geom_col() + theme(axis.text.x = element_text(angle = 90, vjust = 0.3, hjust=1))
+
+
+#tabela1 <- right_join(stevilo_brezposelnih, stevilo_podjetij_ki_so_prenehala_z_delovanjem, )
+starostne.skupine.regije <- right_join(stevilo_hitrorastocih_podjetij, stevilo_zaposlenih_v_podjetjih %>% 
+                                         group_by(regije, leto) %>% summarise(skupaj=sum(stevilo)))
+
+graf1 <- ggplot(starostne.skupine.regije, aes(x=leto, y=stevilo, group=regije, color=regije)) + geom_line() + xlab("Leto") + ylab("Število podjetij, ki so prenehala z delovanjem") + ggtitle("Graf števila propadlih podjetij") + scale_color_discrete(name="Regije", labels=c("Gorenjska", "Goriška", "Jugovzhodna Slovenija", "Koroška", "Obalno-kraška", "Osrednjeslovenska", "Podravska", "Pomurska", "Posavska", "Primorsko-notranjska", "Savinjska", "Zasavska")) + theme(axis.text.x = element_text(angle = 90, vjust = 0.3, hjust=1))
